7f3dba5888dc2f078e672ed62f49c4e3d2da2601
motomen <gdpdp.ru@mail.ru>
2015-06-25 21:59:15 +0300

    some change plugin for JQuery Validation

--- web/SmartValidator.js
+++ web/SmartValidator.js
@@ -16,22 +16,40 @@
 
     function SV (FORM, options) {
         var submit = FORM.find("[type=submit]"); //place for code
+        var valid = false;
 
-        submit.on("click", function() {
-            alert("you click on submit button");
+        FORM.on('submit', function () {
+            if (!valid) {
+                submit.addClass("error");
+            }
+            return (valid);
         });
 
         var allInputs = FORM.find("input");
 
         allInputs.on("keyup", function() {
-            var mail = $(this);
+            var input = $(this);
 
-            if (mail.attr('required')) {
-               if (mail.val() === '') {
-                   mail.addClass("colorError");
+            if (input.attr('required')) {
+               if (input.val() === '') {
+                   valid &= false;
+                   input.addClass("error");
                } else {
-                   mail.removeClass("colorError");
+                   valid = true;
+                   input.removeClass("error");
                }
+            }
+
+            if (input.is("[type=email]")) {
+                var value = input.val();
+                var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+                if(pattern.test(value)) {
+                    valid &= false;
+                    input.addClass("error");
+                }else {
+                    valid = true;
+                    input.removeClass("error");
+                }
             }
         })
     }
